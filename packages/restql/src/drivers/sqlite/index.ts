3736a304104042397f5b78c0e72f595b34c0da78
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2025-08-10 09:09:05 +0200

    lint

--- packages/restql/src/drivers/sqlite/index.ts
+++ packages/restql/src/drivers/sqlite/index.ts
@@ -9,98 +9,98 @@ import { createTable, dropTable, updateTable } from '@/utils/statements'
 import { createOneRaw, findOneRaw, findRaw, removeOneRaw, removeRaw, schemaRaw, updateOneRaw, updateRaw } from './queries'
 
 export default defineDriver(<S extends Schema>(schema: S) => ({
-   findRaw: findRaw(schema) as FindRawFn<S>,
-   findOneRaw: findOneRaw(schema) as FindOneRawFn<S>,
-   updateRaw: updateRaw(schema) as UpdateRawFn<S>,
-   updateOneRaw: updateOneRaw(schema) as UpdateOneRawFn<S>,
-   createOneRaw: createOneRaw(schema) as CreateOneRawFn<S>,
-   removeOneRaw: removeOneRaw(schema) as RemoveOneRawFn<S>,
-   removeRaw: removeRaw(schema) as RemoveRawFn<S>,
-   runTransaction,
-   retrieveSchema,
-   updateSchema,
-   createTableRaw: createTable,
-   updateTableRaw: updateTable,
-   dropTableRaw: dropTable,
+  findRaw: findRaw(schema) as FindRawFn<S>,
+  findOneRaw: findOneRaw(schema) as FindOneRawFn<S>,
+  updateRaw: updateRaw(schema) as UpdateRawFn<S>,
+  updateOneRaw: updateOneRaw(schema) as UpdateOneRawFn<S>,
+  createOneRaw: createOneRaw(schema) as CreateOneRawFn<S>,
+  removeOneRaw: removeOneRaw(schema) as RemoveOneRawFn<S>,
+  removeRaw: removeRaw(schema) as RemoveRawFn<S>,
+  runTransaction,
+  retrieveSchema,
+  updateSchema,
+  createTableRaw: createTable,
+  updateTableRaw: updateTable,
+  dropTableRaw: dropTable
 }), () => createDatabase(connector({ cwd: process.cwd() })))
 
 /**
  * Update the SQLite database schema.
  */
 export async function updateSchema(db: Database, newSchema: Schema) {
-   const current = await retrieveSchema(db)
-   const diff = getSchemaDiff(current, newSchema)
+  const current = await retrieveSchema(db)
+  const diff = getSchemaDiff(current, newSchema)
 
-   return runTransaction(db, async () => {
-      await Promise.all([
-         ...Object.entries(diff.added || {}).map(([table, def]) => db.exec(createTable(table, def))),
-         ...Object.entries(diff.updated || {}).map(([table, def]) => db.exec(updateTable(table, def))),
-         ...Object.entries(diff.removed || {}).map(([table]) => db.exec(dropTable(table))),
-      ])
-   })
+  return runTransaction(db, async () => {
+    await Promise.all([
+      ...Object.entries(diff.added || {}).map(([table, def]) => db.exec(createTable(table, def))),
+      ...Object.entries(diff.updated || {}).map(([table, def]) => db.exec(updateTable(table, def))),
+      ...Object.entries(diff.removed || {}).map(([table]) => db.exec(dropTable(table)))
+    ])
+  })
 }
 
 /**
  * Retrieve the Sqlite database schema.
  */
 export async function retrieveSchema(db: Database) {
-   const query = schemaRaw()
-   const { rows } = await db.sql<{ rows: SchemaRow[] }>`{${query}}`
-   const schema: Schema = {}
+  const query = schemaRaw()
+  const { rows } = await db.sql<{ rows: SchemaRow[] }>`{${query}}`
+  const schema: Schema = {}
 
-   for (const row of rows) {
-      const current = schema[row.table] ??= { columns: {} }
+  for (const row of rows) {
+    const current = schema[row.table] ??= { columns: {} }
 
-      current.columns[row.column] = {
-         type: row.type,
-         notNull: row.notNull === 1,
-         default: row.default || undefined,
-         primaryKey: row.primaryKey === 1,
-         unique: row.primaryKey === 1 ? undefined : row.unique === 1,
-      }
+    current.columns[row.column] = {
+      type: row.type,
+      notNull: row.notNull === 1,
+      default: row.default || undefined,
+      primaryKey: row.primaryKey === 1,
+      unique: row.primaryKey === 1 ? undefined : row.unique === 1
+    }
 
-      if (!row.relationTable) continue
+    if (!row.relationTable) continue
 
-      current.relations ??= {}
+    current.relations ??= {}
 
-      current.relations[row.column] = {
-         table: row.relationTable,
-         fromKey: row.relationFrom,
-         toKey: row.relationTo,
-         onDelete: row.relationOnDelete || 'NO ACTION',
-         onUpdate: row.relationOnUpdate || 'NO ACTION',
-      }
-   }
+    current.relations[row.column] = {
+      table: row.relationTable,
+      fromKey: row.relationFrom,
+      toKey: row.relationTo,
+      onDelete: row.relationOnDelete || 'NO ACTION',
+      onUpdate: row.relationOnUpdate || 'NO ACTION'
+    }
+  }
 
-   return schema
+  return schema
 }
 
 /**
  * Run a transaction.
  */
 export async function runTransaction<T>(db: Database, cb: () => Promise<T>) {
-   await db.exec(`BEGIN TRANSACTION`)
-   try {
-      await cb()
-      await db.exec(`COMMIT`)
-   }
-   catch (error) {
-      await db.exec(`ROLLBACK`)
-      throw error
-   }
+  await db.exec(`BEGIN TRANSACTION`)
+  try {
+    await cb()
+    await db.exec(`COMMIT`)
+  }
+  catch (error) {
+    await db.exec(`ROLLBACK`)
+    throw error
+  }
 }
 
 export interface SchemaRow {
-   table: string
-   column: string
-   type: ColumnTypes
-   notNull?: number
-   default?: string | null
-   primaryKey?: number
-   unique?: number
-   relationTable: string
-   relationFrom: string
-   relationTo: string
-   relationOnUpdate?: RelationOnAction
-   relationOnDelete?: RelationOnAction
+  table: string
+  column: string
+  type: ColumnTypes
+  notNull?: number
+  default?: string | null
+  primaryKey?: number
+  unique?: number
+  relationTable: string
+  relationFrom: string
+  relationTo: string
+  relationOnUpdate?: RelationOnAction
+  relationOnDelete?: RelationOnAction
 }
--- packages/restql/src/drivers/sqlite/queries.ts
+++ packages/restql/src/drivers/sqlite/queries.ts
@@ -11,108 +11,108 @@ import { from, groupBy, insert, joins, limit, offset, orderBy, remove, returning
  * Write a SQL query to find a single record in a table with specified parameters.
  */
 export function findOneRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const P extends FindOneParams<S, T>>(table: T, key: K, params: P) => {
-      const whereClause = addPrimaryKeyCondition(schema, table, key, params)
+  return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const P extends FindOneParams<S, T>>(table: T, key: K, params: P) => {
+    const whereClause = addPrimaryKeyCondition(schema, table, key, params)
 
-      return trim(join([
-         select(schema, table, params.columns as P['columns']),
-         from(table),
-         joins(schema, table, params),
-         where(schema, table, whereClause),
-      ], ' ')) as FindOneRaw<S, T, K, P>
-   }
+    return trim(join([
+      select(schema, table, params.columns as P['columns']),
+      from(table),
+      joins(schema, table, params),
+      where(schema, table, whereClause)
+    ], ' ')) as FindOneRaw<S, T, K, P>
+  }
 }
 
 /**
  * Write a SQL query to find records in a table with specified parameters.
  */
 export function findRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const P extends FindParams<S, T>>(table: T, params: P) => {
-      return trim(join([
-         select(schema, table, params.columns as P['columns']),
-         from(table),
-         joins(schema, table, params),
-         where(schema, table, params.where as P['where']),
-         groupBy(schema, table, params.groupBy as P['groupBy']),
-         orderBy(schema, table, params.orderBy as P['orderBy']),
-         limit(params.limit as P['limit']),
-         offset(params.offset as P['offset']),
-      ], ' ')) as FindRaw<S, T, P>
-   }
+  return <T extends TableName<S>, const P extends FindParams<S, T>>(table: T, params: P) => {
+    return trim(join([
+      select(schema, table, params.columns as P['columns']),
+      from(table),
+      joins(schema, table, params),
+      where(schema, table, params.where as P['where']),
+      groupBy(schema, table, params.groupBy as P['groupBy']),
+      orderBy(schema, table, params.orderBy as P['orderBy']),
+      limit(params.limit as P['limit']),
+      offset(params.offset as P['offset'])
+    ], ' ')) as FindRaw<S, T, P>
+  }
 }
 
 /**
  * Write a SQL query to remove records from a table with specified parameters.
  */
 export function removeRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const P extends RemoveParams<S, T>>(table: T, params: P) => {
-      return trim(join([
-         remove(table),
-         where(schema, table, params.where as P['where']),
-      ], ' ')) as RemoveRaw<S, T, P>
-   }
+  return <T extends TableName<S>, const P extends RemoveParams<S, T>>(table: T, params: P) => {
+    return trim(join([
+      remove(table),
+      where(schema, table, params.where as P['where'])
+    ], ' ')) as RemoveRaw<S, T, P>
+  }
 }
 
 /**
  * Write a SQL query to remove a single record from a table with specified parameters.
  */
 export function removeOneRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const P extends RemoveOneParams<S, T>>(table: T, key: K, params?: P) => {
-      const whereClause = addPrimaryKeyCondition(schema, table, key, params)
+  return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const P extends RemoveOneParams<S, T>>(table: T, key: K, params?: P) => {
+    const whereClause = addPrimaryKeyCondition(schema, table, key, params)
 
-      return trim(join([
-         remove(table),
-         where(schema, table, whereClause),
-      ], ' ')) as RemoveOneRaw<S, T, K, P>
-   }
+    return trim(join([
+      remove(table),
+      where(schema, table, whereClause)
+    ], ' ')) as RemoveOneRaw<S, T, K, P>
+  }
 }
 
 /**
  * Write a SQL query to update records in a table with specified parameters.
  */
 export function updateRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const I extends Partial<Item<S, T>>, const P extends UpdateParams<S, T>>(table: T, item: I, params: P) => {
-      return trim(join([
-         update(table),
-         set(item),
-         where(schema, table, params.where as P['where']),
-      ], ' ')) as UpdateRaw<S, T, I, P>
-   }
+  return <T extends TableName<S>, const I extends Partial<Item<S, T>>, const P extends UpdateParams<S, T>>(table: T, item: I, params: P) => {
+    return trim(join([
+      update(table),
+      set(item),
+      where(schema, table, params.where as P['where'])
+    ], ' ')) as UpdateRaw<S, T, I, P>
+  }
 }
 
 /**
  * Write a SQL query to update a single record in a table with specified parameters.
  */
 export function updateOneRaw<const S extends Schema>(schema: S) {
-   return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const I extends Partial<Item<S, T>>, const P extends UpdateOneParams<S, T>>(table: T, key: K, item: I, params?: P) => {
-      const whereClause = addPrimaryKeyCondition(schema, table, key, params)
+  return <T extends TableName<S>, const K extends PrimaryKeyValue<S, T>, const I extends Partial<Item<S, T>>, const P extends UpdateOneParams<S, T>>(table: T, key: K, item: I, params?: P) => {
+    const whereClause = addPrimaryKeyCondition(schema, table, key, params)
 
-      return trim(join([
-         update(table),
-         set(item),
-         where(schema, table, whereClause),
-      ], ' ')) as UpdateOneRaw<S, T, K, I, P>
-   }
+    return trim(join([
+      update(table),
+      set(item),
+      where(schema, table, whereClause)
+    ], ' ')) as UpdateOneRaw<S, T, K, I, P>
+  }
 }
 
 /**
  * Write a SQL query to create a single record in a table.
  */
 export function createOneRaw<const S extends Schema>(_schema: S) {
-   return <T extends TableName<S>, const I extends Partial<Item<S, T>>>(table: T, item: I) => {
-      return trim(join([
-         insert(table),
-         values(item),
-         returning('*'),
-      ], ' ')) as CreateOneRaw<S, T, I>
-   }
+  return <T extends TableName<S>, const I extends Partial<Item<S, T>>>(table: T, item: I) => {
+    return trim(join([
+      insert(table),
+      values(item),
+      returning('*')
+    ], ' ')) as CreateOneRaw<S, T, I>
+  }
 }
 
 /**
  * Write a SQL query to get the SQLite database schema.
  */
 export function schemaRaw() {
-   return `
+  return `
     SELECT
       sqlite_master.name AS "table",
       meta.name AS "column",
@@ -146,12 +146,12 @@ export function schemaRaw() {
 }
 
 export interface FindParams<S extends Schema = Schema, T extends TableName<S> = TableName<S>> {
-   columns?: FieldName<S, T>[]
-   where?: ConditionTree<S, T>
-   orderBy?: `${'' | '-'}${FieldName<S, T>}`[]
-   groupBy?: FieldName<S, T>[]
-   limit?: number
-   offset?: number
+  columns?: FieldName<S, T>[]
+  where?: ConditionTree<S, T>
+  orderBy?: `${'' | '-'}${FieldName<S, T>}`[]
+  groupBy?: FieldName<S, T>[]
+  limit?: number
+  offset?: number
 }
 
 export type FindOneParams<S extends Schema = Schema, T extends TableName<S> = TableName<S>> = Pick<FindParams<S, T>, 'columns' | 'where'>
@@ -165,49 +165,49 @@ export type UpdateParams<S extends Schema = Schema, T extends TableName<S> = Tab
 export type UpdateOneParams<S extends Schema = Schema, T extends TableName<S> = TableName<S>> = Pick<FindParams<S, T>, 'where'>
 
 export type CreateOneRaw<S extends Schema, T extends TableName<S>, I extends Partial<Item<S, T>>> = Trim<CleanJoin<[
-   Insert<T>,
-   Values<S, T, I>,
-   Returning<['*']>,
+  Insert<T>,
+  Values<S, T, I>,
+  Returning<['*']>
 ], ' '>>
 
 export type UpdateOneRaw<S extends Schema, T extends TableName<S>, K extends PrimaryKeyValue<S, T>, I extends Partial<Item<S, T>>, P extends UpdateOneParams<S, T>> = Trim<CleanJoin<[
-   Update<T>,
-   Set<S, T, I>,
-   Where<S, T, WhereWithPrimaryKey<S, T, K, P>>,
+  Update<T>,
+  Set<S, T, I>,
+  Where<S, T, WhereWithPrimaryKey<S, T, K, P>>
 ], ' '>>
 
 export type UpdateRaw<S extends Schema, T extends TableName<S>, I extends Partial<Item<S, T>>, P extends UpdateParams<S, T>> = Trim<CleanJoin<[
-   Update<T>,
-   Set<S, T, I>,
-   Where<S, T, P['where']>,
+  Update<T>,
+  Set<S, T, I>,
+  Where<S, T, P['where']>
 ], ' '>>
 
 export type RemoveRaw<S extends Schema, T extends TableName<S>, P extends RemoveParams<S, T>> = Trim<CleanJoin<[
-   Remove<T>,
-   Where<S, T, P['where']>,
+  Remove<T>,
+  Where<S, T, P['where']>
 ], ' '>>
 
 export type RemoveOneRaw<S extends Schema, T extends TableName<S>, K extends PrimaryKeyValue<S, T>, P extends RemoveOneParams<S, T>> = Trim<CleanJoin<[
-   Remove<T>,
-   Where<S, T, WhereWithPrimaryKey<S, T, K, P>>,
+  Remove<T>,
+  Where<S, T, WhereWithPrimaryKey<S, T, K, P>>
 ], ' '>>
 
 export type FindRaw<S extends Schema, T extends TableName<S>, P extends FindParams<S, T>> = Trim<CleanJoin<[
-   Select<S, T, P['columns']>,
-   From<S, T>,
-   Joins<S, T, P>,
-   Where<S, T, P['where']>,
-   GroupBy<S, T, P['groupBy']>,
-   OrderBy<S, T, P['orderBy']>,
-   Limit<P['limit']>,
-   Offset<P['offset']>,
+  Select<S, T, P['columns']>,
+  From<S, T>,
+  Joins<S, T, P>,
+  Where<S, T, P['where']>,
+  GroupBy<S, T, P['groupBy']>,
+  OrderBy<S, T, P['orderBy']>,
+  Limit<P['limit']>,
+  Offset<P['offset']>
 ], ' '>>
 
 export type FindOneRaw<S extends Schema, T extends TableName<S>, K extends PrimaryKeyValue<S, T>, P extends FindOneParams<S, T>> = Trim<CleanJoin<[
-   Select<S, T, P['columns']>,
-   From<S, T>,
-   Joins<S, T, P>,
-   Where<S, T, WhereWithPrimaryKey<S, T, K, P>>,
+  Select<S, T, P['columns']>,
+  From<S, T>,
+  Joins<S, T, P>,
+  Where<S, T, WhereWithPrimaryKey<S, T, K, P>>
 ], ' '>>
 
 export type FindRawFn<S extends Schema> = <T extends TableName<S>, const P extends FindParams<S, T>>(table: T, params: P) => FindRaw<S, T, P>
