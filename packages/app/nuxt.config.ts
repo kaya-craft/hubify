30b67b0d45827c2a54d75d0574799a4e0ab49757
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2025-10-31 12:43:05 +0100

    wip

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -13,19 +13,16 @@ export default defineNuxtConfig({
 
   css: [resolve(__dirname, 'app/assets/css/main.css')],
 
-  // routeRules: {
-  //   /**
-  //    * Proxy requests to the Iconify API. Cache responses based on query parameters for 24 hours.
-  //    */
-  //   '/api/iconify/**': {
-  //     proxy: 'https://api.iconify.design/**',
-  //     cache: {
-  //       maxAge: 60 * 60 * 24, // 24 hours
-  //       varies: ['query'],
-  //       swr: true
-  //     }
-  //   }
-  // },
+  routeRules: {
+    '/api/iconify/**': {
+      proxy: 'https://api.iconify.design/**',
+      cache: {
+        maxAge: 60 * 60 * 24,
+        varies: ['query'],
+        swr: true
+      }
+    }
+  },
 
   vite: {
     optimizeDeps: {
